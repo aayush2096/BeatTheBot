e249d11958c907f49976746752252ac7dcee40a2
aayush2096 <aayush2096@gmail.com>
2018-02-13 17:33:50 +0530

    Update app2.js

--- app2.js
+++ app2.js
@@ -33,7 +33,7 @@ var scissorSign=document.getElementById("scissor");
 var game=document.getElementById("gamebt");
 var player=document.getElementById("player_name");
 var restartbt=document.getElementById("restart");
-var choiceClass=document.getElementsByClassName("choicelist");
+
 
 var interval;
 
@@ -201,7 +201,7 @@ startplay.addEventListener('click',function(){
 	
 		},100);
 		startplay.disabled=true;
-		choiceClass.disabled=false;
+		ChoiceClass(false);
 	});
 
 
@@ -210,7 +210,7 @@ rockSign.addEventListener('click',function(){
 	clearInterval(interval);
 	setPlayerSign(signs[2],);
 	getResult("rock");
-	choiceClass.disabled=true;
+	ChoiceClass(true);
 
 });
 paperSign.addEventListener('click',function(){
@@ -218,14 +218,14 @@ paperSign.addEventListener('click',function(){
 	clearInterval(interval);
 	setPlayerSign(signs[0]);
 	getResult("paper");
-	choiceClass.disabled=true;
+	ChoiceClass(true);
 });
 scissorSign.addEventListener('click',function(){
 	clicked=true;
 	clearInterval(interval);
 	setPlayerSign(signs[1]);
 	getResult("scissor");
-	choiceClass.disabled=true;
+	ChoiceClass(true);
 });
 
 function setPlayerSign(playersign)
@@ -233,7 +233,12 @@ function setPlayerSign(playersign)
 	uHand.src=playersign;
 }
 
-
+function ChoiceClass(val)
+	{
+		rockSign.disabled=val;
+		paperSign.disabled=val;
+		scissorSign.disabled=val;
+	}
 
 function getResult(usersign)
 {
