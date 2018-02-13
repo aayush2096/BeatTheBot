7878ebc6e3d43ce79e0cbc812f96a8488b7f5783
aayush2096 <aayush2096@gmail.com>
2018-02-13 11:37:02 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -11,3 +11,16 @@ cache.addAll(staticAssests);
 
 
 });
+
+self.addEventListener('fetch', async event=>{
+
+	const req=event.request;
+	event.respondWith(cachefirst(req));
+
+});
+
+async function cachefirst(req)
+{
+	const cacheRes = await caches.match(req);
+	return cacheRes || fetch(req);
+}
