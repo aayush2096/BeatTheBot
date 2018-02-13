9b7989b3bfebbf4fca1d65ce8329624a6d3b8275
aayush2096 <aayush2096@gmail.com>
2018-02-13 10:27:54 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,13 @@
 
+const staticAssets=[
+"./",
+"./btb.css",
+"./app2.js"
+];
+
+self.addEventListener('install', async function()
+{
+
+	const cache = await caches.open('game-static');
+	cache.addAll(staticAssets);
+});
